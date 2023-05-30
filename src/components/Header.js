69dc366a10e447644623b0efb22fc64e0759653b
profrbazur <profrbazurph@gmail.com>
2023-05-31 06:09:25 +0800

    KodeShop Context Reducer Implementation

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../assets/logo.png";
+import { useCart } from "../context/CartContext";
+import "./Header.css";
+
+export const Header = () => {
+  const { cartList } = useCart();
+
+  return (
+    <header>
+      <Link
+        to="/"
+        className="logo"
+      >
+        <img
+          src={Logo}
+          alt="Kodego Logo"
+        />
+        <span>KodeShop</span>
+      </Link>
+      <nav className="navigation">
+        <NavLink
+          to="/"
+          className="link"
+          end
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/cart"
+          className="link"
+        >
+          Cart
+        </NavLink>
+      </nav>
+      <Link
+        to="/cart"
+        className="items"
+      >
+        <span>Cart: {cartList.length}</span>
+      </Link>
+    </header>
+  );
+};
